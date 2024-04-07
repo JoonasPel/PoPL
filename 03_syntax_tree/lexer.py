7b830edd4178f69d0a27e42ef3b04160879e58bf
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-04-08 02:11:12 +0300

    Phase 3 init

---
+++ 03_syntax_tree/lexer.py
@@ -0,0 +1,134 @@
+import sys, ply.lex, datetime
+
+STUDENTNAME = "Joonas Pelttari"
+STUDENTID = "H274830"
+HELPMESSAGE = '''
+usage: main.py [-h] [--who | -f FILE]
+  -h, --help            show this help message and exit
+  --who                 print out student IDs and NAMEs of authors
+  -f FILE, --file FILE  filename to process
+'''
+
+reserved = {
+    'var': 'VAR',
+    'is': 'IS',
+    'unless': 'UNLESS',
+    'otherwise': 'OTHERWISE',
+    'until': 'UNTIL',
+    'do': 'DO',
+    'done': 'DONE',
+    'procedure': 'PROCEDURE',
+    'function': 'FUNCTION',
+    'return': 'RETURN',
+    'print': 'PRINT',
+    'end': 'END',
+}
+
+tokens = [
+    'LPAREN','RPAREN','LSQUARE','RSQUARE','LCURLY',
+    'RCURLY','APOSTROPHE','AMPERSAND','COMMA','DOT',
+    'EQ','LT','PLUS','MINUS','MULT','DIV','INT_LITERAL',
+    'STRING', 'COMMENT', 'IDENT', 'DATE_LITERAL',
+    'FUNC_IDENT', 'PROC_IDENT',
+    ]
+tokens += reserved.values()
+
+# one and two letter tokens:
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LSQUARE = r'\['
+t_RSQUARE = r'\]'
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
+
+t_APOSTROPHE = r'\''
+t_AMPERSAND = r"\&"
+t_COMMA = r','
+t_DOT = r'\.'
+t_EQ = r'\='
+t_LT = r'\<'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULT = r'\*'
+t_DIV = r'/'
+
+# longer tokens
+
+
+def t_STRING(t):
+    r'"([^"]*)"'
+    t.value = t.value.replace('"', '')
+    return t
+
+def t_DATE_LITERAL(t):
+    r'\d{4}-\d{2}-\d{2}'
+    try:
+        t.value = datetime.date(
+            int(t.value[0:4]), int(t.value[5:7]), int(t.value[8:10]))
+    except ValueError:
+        print(f"line {t.lexer.lineno}: Invalid Date")
+        sys.exit(1)
+    return t
+
+def t_INT_LITERAL(t):
+    r'-?\d{1,3}(\'\d{3})*'
+    t.value = t.value.replace("'", "")
+    t.value = int(t.value)
+    if abs(t.value) >= 1_000_000_000_000:
+        print(f"line {t.lexer.lineno}: INT_LITERAL too large")
+        sys.exit(1)
+    return t
+
+def t_COMMENT(t):
+    r'\(%[^%]*%\)'
+    # comments can span to multiple lines. This keeps count of correct line num
+    t.lexer.lineno += t.value.count("\n")
+    pass
+
+def t_IDENT(t):
+    r'[a-z][a-zA-Z0-9_]{1,}'
+    if t.value in reserved:
+        t.type = reserved[t.value]
+    return t
+
+def t_FUNC_IDENT(t):
+    r'[A-Z][a-z0-9_]{1,}'
+    return t
+
+def t_PROC_IDENT(t):
+    r'[A-Z]{2}[A-Z0-9_]*'
+    return t
+
+t_ignore = " "
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    raise Exception("Illegal character '{}' at line {}".format( 
+        t.value[0], t.lexer.lineno ) )
+
+lexer = ply.lex.lex()
+
+
+def handleArguments(args):
+    if '-h' in args or '--help' in args:
+        print(HELPMESSAGE)
+    elif '--who' in args:
+        print(STUDENTNAME, STUDENTID)
+    elif '-f' in args or '--file' in args:
+        return args[-1]
+    else:
+        print(HELPMESSAGE)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    fileName = handleArguments(sys.argv)
+    with open(fileName, 'r', encoding='utf-8') as file:
+        data = file.read()
+    lexer.input(data)
+    token = lexer.token()
+    while token:
+        print(token)
+        token = lexer.token()
---
+++ 03_syntax_tree/main.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+import ply.yacc
+import ply.lex
+import lexer # previous phase example snippet code
+
+# Simple debuggin function to call from syntax rules
+symbolnum=0
+def debug_syntax(p):
+  global symbolnum
+  symbolnum=symbolnum+1
+  p[0] = symbolnum
+  msg = ""
+  for i,s in enumerate(p.slice):
+    if s is not None:
+      if type(s) is ply.lex.LexToken:
+        msg = msg + str(s.type)+"<" + str(s.value) + "> "
+      else:
+        msg = msg + str(s)+"("+str(p[i])+")" + " "
+    else:
+      msg = msg + "?? "
+    if i == 0:
+      msg = msg + ":: "
+  print(msg)
+
+# tokens are defined in lex-module, but needed here also in syntax rules
+tokens = lexer.tokens
+
+# any function starting with 'p_' is PLY yacc rule
+# first definition is the target we want to reduce
+# in other words: after processing all input tokens, if this start-symbol
+# is the only one left, we do not have any syntax errors
+def p_program(p):
+    '''program : opt_definitions statement_list'''
+    debug_syntax(p)
+
+def p_empty(p):
+    '''empty : '''
+    debug_syntax(p)
+
+def p_statement_list(p):
+    '''statement_list : statement
+                      | statement COMMA statement_list'''
+    debug_syntax(p)
+
+def p_definitions(p):
+    '''definitions : function_definition
+                   | procedure_definition
+                   | variable_definition'''
+    debug_syntax(p)
+
+def p_opt_definitions(p):
+    '''opt_definitions : opt_definitions definitions
+                       | empty'''
+    debug_syntax(p)
+
+def p_variable_definition(p):
+    '''variable_definition : VAR IDENT EQ expression'''
+    debug_syntax(p)
+
+def p_opt_var_defs(p):
+    '''opt_var_defs : var_def_list
+                    | empty'''
+    debug_syntax(p)
+
+def p_var_def_list(p):
+    '''var_def_list : var_def_list variable_definition
+                    | variable_definition
+                    | empty'''
+    debug_syntax(p)
+
+def p_function_definition(p):
+    '''function_definition : FUNCTION FUNC_IDENT LCURLY opt_formals RCURLY RETURN IDENT opt_var_defs IS rvalue END FUNCTION'''
+    debug_syntax(p)
+
+def p_procedure_definition(p):
+    '''procedure_definition : PROCEDURE PROC_IDENT LCURLY opt_formals RCURLY opt_return_type opt_var_defs IS statement_list END PROCEDURE'''
+    debug_syntax(p)
+
+def p_opt_return_type(p):
+    '''opt_return_type : RETURN IDENT
+                       | empty'''
+    debug_syntax(p)
+
+def p_formals(p):
+    '''formals : formal_arg
+               | formals COMMA formal_arg'''
+    debug_syntax(p)
+
+def p_opt_formals(p):
+    '''opt_formals : formals
+                   | empty'''
+    debug_syntax(p)
+
+def p_formal_arg(p):
+    '''formal_arg : IDENT LSQUARE IDENT RSQUARE'''
+    debug_syntax(p)
+
+def p_procedure_call(p):
+    '''procedure_call : PROC_IDENT LPAREN opt_args RPAREN'''
+    debug_syntax(p)
+
+def p_args(p):
+    '''args : expression
+            | args COMMA expression'''
+    debug_syntax(p)
+
+def p_opt_args(p):
+    '''opt_args : args
+                | empty'''
+    debug_syntax(p)
+
+def p_assignment(p):
+    '''assignment : lvalue EQ rvalue
+                  | IDENT DOT IDENT'''
+    debug_syntax(p)
+
+def p_lvalue(p):
+    '''lvalue : IDENT
+              | IDENT DOT IDENT'''
+    debug_syntax(p)
+
+def p_rvalue(p):
+    '''rvalue : expression
+              | unless_expression'''
+    debug_syntax(p)
+
+def p_print_statement(p):
+    '''print_statement : PRINT printlist'''
+    debug_syntax(p)
+
+def p_printlist(p):
+    '''printlist : printitem
+                 | printlist AMPERSAND printitem'''
+    debug_syntax(p)
+
+def p_printitem(p):
+    '''printitem : STRING
+                  | expression'''
+    debug_syntax(p)
+
+def p_statement(p):
+    '''statement : procedure_call
+                 | assignment
+                 | print_statement
+                 | unless_statement
+                 | loop_statement
+                 | RETURN expression'''
+    debug_syntax(p)
+
+def p_loop_statement(p):
+    '''loop_statement : DO statement_list UNTIL expression'''
+    debug_syntax(p)
+
+def p_unless_statement(p):
+    '''unless_statement : DO statement_list UNLESS expression opt_otherwise DONE'''
+    debug_syntax(p)
+
+def p_opt_otherwise(p):
+    '''opt_otherwise : OTHERWISE statement_list
+                     | empty'''
+    debug_syntax(p)
+
+def p_expression(p):
+    '''expression : simple_expr
+                  | expression relation_op simple_expr'''
+    debug_syntax(p)
+
+def p_relation_op(p):
+    '''relation_op : EQ
+                   | LT'''
+    debug_syntax(p)
+
+def p_simple_expr(p):
+    '''simple_expr : term
+                   | simple_expr add_or_minus term'''
+    debug_syntax(p)
+
+def p_add_or_minus(p):
+    '''add_or_minus : PLUS
+                    | MINUS'''
+    debug_syntax(p)
+
+def p_mult_or_div(p):
+    '''mult_or_div : MULT
+                   | DIV'''
+    debug_syntax(p)
+
+def p_term(p):
+    '''term : factor
+            | term mult_or_div factor'''
+    debug_syntax(p)
+
+def p_factor(p):
+    '''factor : atom
+              | PLUS atom  
+              | MINUS atom'''
+    debug_syntax(p)
+
+def p_atom(p):
+    '''atom : IDENT
+            | IDENT APOSTROPHE IDENT
+            | INT_LITERAL
+            | DATE_LITERAL
+            | function_call
+            | procedure_call
+            | LPAREN expression RPAREN'''
+    debug_syntax(p)
+
+def p_function_call(p):
+    '''function_call : FUNC_IDENT LPAREN opt_args RPAREN'''
+    debug_syntax(p)
+
+def p_unless_expression(p):
+    '''unless_expression : DO expression UNLESS expression OTHERWISE expression DONE'''
+    debug_syntax(p)
+
+def p_error(p):
+    if (p):
+        print( f"{p.lineno}:Syntax Error (token: '{p.value}')" )
+    else:
+        print("Unexpected end of input")
+    raise SystemExit
+
+parser = ply.yacc.yacc()
+
+if __name__ == '__main__':
+    import argparse, codecs
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-d', '--debug', action='store_true', help='debug?' )
+    group = arg_parser.add_mutually_exclusive_group()
+    group.add_argument('--who', action='store_true', help='who wrote this' )
+    group.add_argument('-f', '--file', help='filename to process')
+    ns = arg_parser.parse_args()
+    Debug = True if ns.debug else False
+    if ns.who == True:
+        # identify who wrote this
+        print( 'H274830 Joonas Pelttari' )
+    elif ns.file is None:
+        # user didn't provide input filename
+        arg_parser.print_help()
+    else:
+        data = codecs.open( ns.file, encoding='utf-8' ).read()
+        result = parser.parse(data, lexer=lexer.lexer, debug=Debug )
+        print( 'syntax OK' )
+
